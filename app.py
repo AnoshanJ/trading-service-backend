76d8afa7b4f44b8b8e950dc63deabd377e8d1f38
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2024-04-12 23:28:08 +0530

    Fix syntax error

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def add_trade():
         'quantity': data['quantity'],
         'trade_type': data['trade_type']  # 'buy' or 'sell'
     }
-trades.append(trade)
+    trades.append(trade)
     return jsonify(trade), 201
 
 
